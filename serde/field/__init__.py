d739fc3ee71412707a1bcc20f2b1783fe4eb1fba
Ross MacArthur <macarthur.ross@gmail.com>
2018-11-09 22:54:01 +0200

    Fix lints and tests

--- serde/field/__init__.py
+++ serde/field/__init__.py
@@ -5,6 +5,7 @@ Field types for Serde Models.
 from .core import Bool, Dict, Field, Float, InstanceField, Int, List, ModelField, Str, Tuple
 from .ext import Choice, Domain, Email, Slug, Url, Uuid
 
+
 __all__ = [
     'Bool', 'Boolean', 'Choice', 'Dict', 'Dictionary', 'Domain', 'Email', 'Field', 'Float',
     'InstanceField', 'Int', 'Integer', 'List', 'ModelField', 'Slug', 'Str', 'String', 'Tuple',
