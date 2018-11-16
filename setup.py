3b2729ba427344945b320306f06326e66a3a876a
Ross MacArthur <macarthur.ross@gmail.com>
2018-11-16 15:00:45 +0200

    Update setup.py and README (#25)

--- setup.py
+++ setup.py
@@ -34,39 +34,38 @@ url = 'https://github.com/rossmacarthur/serde'
 
 long_description = read('README.rst')
 
-install_requirements = [
+install_requires = [
     'validators>=0.12.0<0.13.0'
 ]
 
-toml_requirements = [
+toml_requires = [
     'toml>=0.10.0<0.11.0'
 ]
 
-yaml_requirements = [
+yaml_requires = [
     'ruamel.yaml>=0.15.0<0.16.0'
 ]
 
-lint_requirements = [
+lint_requires = [
     'flake8',
     'flake8-docstrings',
     'flake8-isort',
     'flake8-per-file-ignores',
     'flake8-quotes',
-    'mccabe',
     'pep8-naming'
 ]
 
-test_requirements = [
+test_requires = [
     'pytest>=3.3.0',
     'pytest-cov',
     'pytest-doctest-import'
 ]
 
-document_requirements = [
+document_requires = [
     'sphinx'
 ]
 
-package_requirements = [
+package_requires = [
     'twine'
 ]
 
@@ -74,20 +73,23 @@ setup(
     name='serde',
     packages=find_packages(exclude=['docs', 'tests']),
     version=version,
-    install_requires=install_requirements,
+    install_requires=install_requires,
     extras_require={
-        'toml': toml_requirements,
-        'yaml': yaml_requirements,
-        'linting': lint_requirements,
-        'testing': test_requirements,
-        'documenting': document_requirements,
-        'packaging': package_requirements
+        'toml': toml_requires,
+        'yaml': yaml_requires,
+        'linting': lint_requires,
+        'testing': test_requires,
+        'documenting': document_requires,
+        'packaging': package_requires
     },
     python_requires='>=3.4',
 
     author='Ross MacArthur',
     author_email='macarthur.ross@gmail.com',
-    description='A framework for serializing and deserializing Python objects.',
+    maintainer='Ross MacArthur',
+    maintainer_email='macarthur.ross@gmail.com',
+    description=('A lightweight, general-purpose, ORM framework for defining, serializing, '
+                 'deserializing, and validating data structures.'),
     long_description=long_description,
     license='MIT',
     keywords='serde serialization deserialization schema json',
@@ -102,6 +104,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ]
 )
