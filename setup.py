adbd1c08f510f5958865893a30dbb656195965d5
Ross MacArthur <ross@macarthur.io>
2019-01-06 20:08:19 +0200

    Remove py_modules from setup

--- setup.py
+++ setup.py
@@ -75,7 +75,6 @@ setup(
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    py_modules=['serde'],
 
     # Metadata
     download_url='{url}/archive/{version}.tar.gz'.format(**metadata),
