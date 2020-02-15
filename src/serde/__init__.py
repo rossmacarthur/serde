6d1db18c0743e33660b240c7aec845c841e7b2ea
Ross MacArthur <ross@macarthur.io>
2020-02-15 18:54:17 +0200

    This 0.8.0-beta.1

--- src/serde/__init__.py
+++ src/serde/__init__.py
@@ -9,7 +9,7 @@ from serde.model import Model
 
 __all__ = ['Model', 'ValidationError', 'fields', 'tags', 'validators']
 __title__ = 'serde'
-__version__ = '0.7.3'
+__version__ = '0.8.0-beta.1'
 __url__ = 'https://github.com/rossmacarthur/serde'
 __author__ = 'Ross MacArthur'
 __author_email__ = 'ross@macarthur.io'
